1c05f0235aec87a966962921c8c45e085aef60bd
ankit7890mn <78220892+ankit7890mn@users.noreply.github.com>
2021-04-20 09:24:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -89,6 +89,15 @@ function keyPressed()
 	}
       
       Matter.Body.translate(packageBody , {x:-20,y:0});
+	
+	
+	if(keyCode === RIGHT_ARROW)
+	{
+		helicopterSprite.x=helicopterSprite.x+20;
+		
+	}
+	
+	  Matter.Body.translate(packageBody , {x:-20,y:0});
 
 	if(keyCode === DOWN_ARROW)
 	{
